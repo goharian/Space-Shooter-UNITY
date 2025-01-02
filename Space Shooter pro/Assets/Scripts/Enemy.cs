276fac75fb9fd694041f481f630fd4af0cd38fdd
goharian <matanya.goharian@gmail.com>
2025-01-02 12:06:21 +0200

    Initial Commit

---
+++ Space Shooter pro/Assets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.position = new Vector3(0, 8, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move down at 4 meters pre sec
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+        
+
+        //if bottom of screen
+        if (transform.position.y < -5.5f)
+        {
+            //respawn at top (bunos: with a new random x position)
+            float randomX = Random.Range(-9.5f, 9.5f);
+            transform.position = new Vector3(randomX, 7.0f, 0);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("Player")){
+            Player player = other.transform.GetComponent<Player>();
+            if(player != null){
+                player.Damage();
+            }
+
+            Destroy(this.gameObject);
+        }
+
+        if(other.gameObject.CompareTag("Laser")){
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Space Shooter pro/Assets/Scripts/Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.2f;
+    private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalaulateMovement();
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void CalaulateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        if (transform.position.y >= 6)
+        {
+            transform.position = new Vector3(transform.position.x, 6, 0);
+        }
+        else if (transform.position.y < -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+
+        if (transform.position.x > 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
+    } 
+
+    void FireLaser()
+    {
+            _canFire = Time.time + _fireRate;
+            Vector3 position = transform.position + new Vector3(0, 0.8f, 0);
+            Instantiate(_laserPrefab, position, Quaternion.identity);
+    }
+
+    public void Damage(){
+        _lives--;
+        if(_lives < 1){
+            Destroy(this.gameObject);
+        }
+    }
+}
